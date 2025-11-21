1ab2e7557d61555bd905bdf1ace4666b3a9dd12f
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-11-21 22:20:27 +0600

    update sha256

--- Formula/system-suite.rb
+++ Formula/system-suite.rb
@@ -2,7 +2,7 @@ class SystemSuite < Formula
   desc "Terminal-based system maintenance and monitoring toolkit"
   homepage "https://github.com/hamim-24/system_suite"
   url "https://github.com/hamim-24/system_suite/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "e9509728f4a9bf3f2f4efced44b5fb67d39f3bf92870528fee95ea5bd956a2d1"
   license "MIT"
   version "1.0.5"
 
