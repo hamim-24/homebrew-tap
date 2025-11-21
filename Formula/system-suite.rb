86eb418ff87f3fb7d21b97d8d833baa4a2c8e84b
Md Inzamamul Lohani <hamimlohani@gmail.com>
2025-11-21 22:15:38 +0600

    upd

--- Formula/system-suite.rb
+++ Formula/system-suite.rb
@@ -1,7 +1,7 @@
 class SystemSuite < Formula
   desc "Terminal-based system maintenance and monitoring toolkit"
   homepage "https://github.com/hamim-24/system_suite"
-  url "https://github.com/hamim-24/system_suite/archive/refs/tags/v1.0.4.tar.gz"
+  url "https://github.com/hamim-24/system_suite/archive/refs/tags/v1.0.5.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
   version "1.0.5"
